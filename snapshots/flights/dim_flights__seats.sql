35024c2564507fff7459607f77e51d0b728139cc
Alex <alexkipr@mail.ru>
2025-02-05 07:02:27 +0300

    Неделя 6. Конфигурирование моделей. Configs

--- snapshots/flights/dim_flights__seats.sql
+++ snapshots/flights/dim_flights__seats.sql
@@ -3,10 +3,10 @@
 {{
    config(
        target_schema='snapshot',
-       unique_key='aircraft_code',
+       unique_key=['aircraft_code', 'seat_no'],
 
        strategy='check',
-       check_cols = ['aircraft_code', 'seat_no', 'fare_conditions']
+       check_cols = ['fare_conditions']
    )
 }}
 
