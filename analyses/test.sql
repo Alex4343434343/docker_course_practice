cb2789dadb17135d89bf14d4e85937b98eb89658
Alex <alexkipr@mail.ru>
2025-03-16 03:15:26 +0300

    Тест

--- analyses/test.sql
+++ analyses/test.sql
@@ -1,3 +1,7 @@
-SELECT DISTINCT
-    status
-FROM {{ ref('stg_flights__flights') }}
+{% set source_relation = load_relation( ref("stg_flights__flights")) %}
+
+{% set columns = adapter.get_columns_in_relation(source_relation) %}
+
+{% for column in columns -%}
+    {{'Columns: ' ~ column }}
+{% endfor %}
